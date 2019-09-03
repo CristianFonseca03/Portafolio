727c9eec94f1a89ca0eb54b99624a8b231b23ce8
Cristian Fonseca <cristian.lfs@gmail.com>
2019-09-03 15:07:55 -0500

    added moment.js

--- pruebas/script.js
+++ pruebas/script.js
@@ -1,27 +1,23 @@
 function get_date() {
-    var d = new Date();
-    var strDate = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-    return strDate;
+    return moment().format('h:mm:ss a');;
 };
 
 function alter_date() {
     $("#fecha").html(get_date());
 };
 
-function alter_time_elapsed(h, m, s) {
-    $("#tt_carga").html(time_elapsed(h, m, s));
+function alter_time_elapsed() {
+    var sec = parseInt($("#seconds_elapsed").text());
+    sec++;
+    $("#seconds_elapsed").html(sec);
+    var formatted = moment.utc(sec * 1000).format('HH:mm:ss');
+    $("#tt_carga").html(formatted);
 };
 
-function time_elapsed(h, m, s) {
-    var d = new Date();
-    ch = Math.abs(d.getHours() - h);
-    cm = Math.abs(d.getMinutes() - m);
-    cs = Math.abs(d.getSeconds() - s);
-    return ch + ":" + cm + ":" + cs;
-};
 $(function () {
-    var ct = new Date();
-    setInterval('alter_date()', 150);
-    $("#carga").html(ct.getHours() + ":" + ct.getMinutes() + ":" + ct.getSeconds());
-    setInterval('alter_time_elapsed(11, 30, 00)', 150);
+    moment.locale('es');
+    setInterval('alter_date()', 100);
+    $("#carga").html(get_date());
+    console.log(moment([2010, 1, 14, 15, 25, 50]).format('LLLL'));
+    setInterval('alter_time_elapsed()', 1000);
 });
