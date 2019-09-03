6e72e067f397c597164f748529c50f9982bec6c8
JUANDA-07 <juan.caceres02@uptc.edu.co>
2019-09-03 13:53:13 -0500

    Revert "added moment.js"
    This reverts commit fc009e72d5f7e86d45ffb62215689eec2eac77e5.

--- pruebas/script.js
+++ pruebas/script.js
@@ -1,23 +1,27 @@
 function get_date() {
-    return moment().format('h:mm:ss a');;
+    var d = new Date();
+    var strDate = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+    return strDate;
 };
 
 function alter_date() {
     $("#fecha").html(get_date());
 };
 
-function alter_time_elapsed() {
-    var sec = parseInt($("#seconds_elapsed").text());
-    sec++;
-    $("#seconds_elapsed").html(sec);
-    var formatted = moment.utc(sec * 1000).format('HH:mm:ss');
-    $("#tt_carga").html(formatted);
+function alter_time_elapsed(h, m, s) {
+    $("#tt_carga").html(time_elapsed(h, m, s));
 };
 
+function time_elapsed(h, m, s) {
+    var d = new Date();
+    ch = Math.abs(d.getHours() - h);
+    cm = Math.abs(d.getMinutes() - m);
+    cs = Math.abs(d.getSeconds() - s);
+    return ch + ":" + cm + ":" + cs;
+};
 $(function () {
-    moment.locale('es');
-    setInterval('alter_date()', 100);
-    $("#carga").html(get_date());
-    console.log(moment([2010, 1, 14, 15, 25, 50]).format('LLLL'));
-    setInterval('alter_time_elapsed()', 1000);
+    var ct = new Date();
+    setInterval('alter_date()', 150);
+    $("#carga").html(ct.getHours() + ":" + ct.getMinutes() + ":" + ct.getSeconds());
+    setInterval('alter_time_elapsed(11, 30, 00)', 150);
 });
